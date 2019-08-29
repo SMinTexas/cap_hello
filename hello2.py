98c2611ae4e11e5f051099a111c7bd48b03d84ae
Steven Murray <tstevenm@gmail.com>
2019-08-29 14:59:36 -0500

    Initial Commit

---
+++ hello2.py
@@ -0,0 +1,9 @@
+#Prompt for a user's name
+#Print the entered name in CAPS
+#Count the number of characters in the name and print that out as well
+
+name = input('WHAT IS YOUR NAME? ')
+char_count = len(name)
+
+print(f'HELLO, {name}!')
+print(f'YOUR NAME HAS {char_count} LETTERS IN IT! AWESOME!')
